7025dc08c08394747974ebed8789f582910cbe92
Thong Nguyn <thongdatng201@gmail.com>
2025-03-03 21:13:14 +0700

    Add project files.

---
+++ UseCases/IToDoItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCases
+{
+    public interface IToDoItemRepository
+    {
+        void AddTodoItem(ToDoItem item);
+        ToDoItem GetById(int id);
+        IEnumerable<ToDoItem> GetTodoItems();
+        void Update(object item);
+    }
+}
---
+++ UseCases/ToDoListManager.cs
@@ -0,0 +1,34 @@
+﻿using Entities;
+
+namespace UseCases
+{
+    public class ToDoListManager
+    {
+        private readonly IToDoItemRepository repository;
+        public ToDoListManager(IToDoItemRepository repository)
+        {
+            this.repository = repository;
+        }
+        public IEnumerable<ToDoItem> getTodoItems()
+        {
+            return repository.GetTodoItems();
+        }
+
+        public void AddTodoItem(ToDoItem item)
+        {
+            repository.AddTodoItem(item);
+        }
+
+        public void MarkComplete(int id)
+        {
+            var item = repository.GetById(id);
+            if (item != null)
+            {
+                item.IsCompleted = true;
+                
+            }
+            repository.Update(item);
+
+        }
+    }
+}
